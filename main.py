bf3053e0cfba851b4b86b89eeaa5c71dcf3824a2
Eivind Chen <112402146+FrostyAqua37@users.noreply.github.com>
2026-03-12 21:15:43 +0100

    Webscraped currencies and their BTC exchange rates.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
 conversion_values = dict()
 
 content = requests.get('https://bitcoinlive.org/')
 soup = BeautifulSoup(content.text,'html.parser')
+currency = []
 
-a = soup.find_all('ul', {'class': 'cur-list'})
-b = []
-for i in a:
-    b.append(i.get_text())
+def remove_tags(raw_html:str) -> str:
+    pattern = re.compile('<.*?>')
+    return re.sub(pattern, '', raw_html)
+
+#All bitcoin exchange rate between currencies.
+for il in soup.find_all('ul', class_ = 'cur-list'):
+    currency.append(il.find_all('span'))
+
+
+for country in currency:
+    print(remove_tags(str(country)).strip())
+
+
 
