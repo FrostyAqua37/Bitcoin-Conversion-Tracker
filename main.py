b7d34cb84c4c0fcda9d7af19d420fa0956af420e
FrostyAqua37 <eivindc@uia.no>
2026-03-15 18:06:18 +0100

    Got most of the BTC Exchange rates with some exceptions.

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 
-conversion_values = dict()
+exchange_rates: dict[str, str] = dict()
+indices: list[str] = []
 
-content = requests.get('https://bitcoinlive.org/')
-soup = BeautifulSoup(content.text,'html.parser')
-currency = []
+def html_content(url:str) -> object:
+    page = requests.get(url)
+    soup = BeautifulSoup(page.text, 'html.parser')
+    return soup
 
-def remove_tags(raw_html:str) -> str:
-    pattern = re.compile('<.*?>')
-    return re.sub(pattern, '', raw_html)
+def get_conversion_table(table:object) -> list[str]:
+    return table.find_all('span', class_ = 'pull-right')[8:]
 
-#All bitcoin exchange rate between currencies.
-for il in soup.find_all('ul', class_ = 'cur-list'):
-    currency.append(il.find_all('span'))
+def remove_html(content:list[str]) -> list[str]:
+    new_list:list[str] = []
+
+    for row in content:
+        new_list.append(row.text.strip())
+
+    return new_list
+
+def get_exchange_rates(table:list[str]):
+    for row in table:
+        try:
+            country, rate = row.split('\xa0')
+
+            exchange_rates[country] = rate
+        except:
+            indices.append(row)
+
+def main():
+    html_page = html_content('https://bitcoinlive.org/')
+    exchange_table = get_conversion_table(html_page)
+    exchange_table = remove_html(exchange_table)
+    get_exchange_rates(exchange_table)
 
 
-for country in currency:
-    print(remove_tags(str(country)).strip())
 
-
+if __name__ == '__main__':
+    main()
 
