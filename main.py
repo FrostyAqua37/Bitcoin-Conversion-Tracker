e8083b3396cb38e95ed64584fb030b65ee930801
FrostyAqua37 <eivindc@uia.no>
2026-03-20 16:13:43 +0100

    Created a CSV file with bitcoin value between all currencies.

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 import re
-
-exchange_rates: dict[str, str] = dict()
-indices: list[str] = []
 
 def html_content(url:str) -> object:
     page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    return soup
+    return BeautifulSoup(page.text, 'html.parser')
 
 def get_conversion_table(table:object) -> object:
     return table.find_all('div', class_ = 'col-full-flex')[2]
 
-def get_exchange_rates(exchange_table:object) -> list[str]:
+def get_currency_values(exchange_table:object) -> list[str]:
     #Finds all exchange rate between all currencies
     values = exchange_table.find_all('span', class_ = 'pull-right')
     values = [value.text.replace('\xa0', ' ') for value in values] #Removes any HTML tags and creates space between code and currency value
-    return [value.split(' ')[3].strip() for value in values]
+    return [value.split(' ')[-2] for value in values]
 
 def get_currency_name(exchange_table:object) -> list[str]:
     #Finds every <a> tag in given table
@@ -26,18 +23,31 @@ def get_currency_name(exchange_table:object) -> list[str]:
 
     return [code.split('-')[1].strip() for code in country_code] #Splits at '-' and only returns currency name.
 
-def remove_tags(html_content:object) -> list[str]:
-    ...
+def get_bitcoin_df(names, values) -> pd.DataFrame:
+    df = pd.DataFrame(columns = ['currency_name', 'currency_value'])
 
-def format_text(text_content:list[str]) -> list[str]:
-    ...
+    for i in range(len(names)):
+        #Formats value and creates dictionary of exchange rates
+        values[i] = keep_number(values[i])
+        row = [names[i], values[i]]
 
+        df.loc[i] = row
+
+    return df
+
+
+def keep_number(string:str) -> str:
+    # Removing any non-numeric characters but keeps commas and dots
+    return re.sub("[^0-9,.]", "", string)
 
 def main():
     html_page = html_content('https://bitcoinlive.org/')
     conversion_table = get_conversion_table(html_page)
-    currency_name = get_currency_name(conversion_table)
+    currency_names = get_currency_name(conversion_table)    #List over currency names
+    currency_values = get_currency_values(conversion_table) #List over currency value for 1 bitcoin.
 
+    bitcoin_df = get_bitcoin_df(currency_names, currency_values)
+    bitcoin_df.to_csv('bitcoin_df.csv', index=False)
 
 if __name__ == '__main__':
     main()
